4a69840ab61a7744c1bcae5ca4b21bc7a783c9ab
Aldy Muldani <dieabra@gmail.com>
2020-04-30 04:33:35 +0700

    push

--- application/controllers/Api/ApiBarang.php
+++ application/controllers/Api/ApiBarang.php
@@ -11,80 +11,98 @@ Class ApiBarang extends CI_Controller{
         $this->load->Model('Model_barang');
     }
 
-     public function index()
-     {
-        $data = $this->Model_barang->show_barang()->result();
-        if ($this->input->get('id_barang') != '') {
-        	$id_barang = $this->input->get('id_barang');
-	        $data = $this->Model_barang->show_one_barang($id_barang)->result();
-        }
-        echo json_encode($data);
-     }
+    public function index()
+    {
+       $req = $_SERVER['REQUEST_METHOD'];
+       
+       switch ($req) {
+          case 'GET':
+               $data = $this->Model_barang->show_barang()->result();
+               if ($this->input->get('id_barang') != '') {
+                  $id_barang = $this->input->get('id_barang');
+                  $data = $this->Model_barang->show_one_barang($id_barang)->result();
+               }
+               echo json_encode($data);
+            break;
+
+          case 'POST':
+               $data = $this->Model_barang->add();
+
+               http_response_code(500);
+               $arrResult = array(
+                  'result' => false,
+                  'code' => 500,
+                  'message' => 'Internal Server Error'
+               );
+
+               if ($data == 1) {
+                  http_response_code(201);
+                  $arrResult = array(
+                     'result' => true,
+                     'code' => 201,
+                     'message' => 'Data Was Created'
+                  );         
+         
+               }
+               echo json_encode($arrResult);
+             break;
+
+          case 'PUT':
+               $data = $this->Model_barang->update();
+               http_response_code(404);
+               $arrResult = array(
+                  'result' => false,
+                  'code' => 404,
+                  'message' => 'Data Not Found'
+               );
+               $barang = $this->Model_barang->show_one_barang($this->input->post('id_barang'))->result();         
+               if (count($barang) == 1) {
+                  http_response_code(200);
+                  $arrResult = array(
+                     'result' => true,
+                     'code' => 200,
+                     'message' => 'Data Was Updated'
+                  );
+               }
+               echo json_encode($arrResult);
+
+             break;
+
+          case 'Delete':
+               http_response_code(404);
+               $arrResult = array(
+                  'result' => false,
+                  'code' => 404,
+                  'message' => 'Data Not Found'
+               );
+               $barang = $this->Model_barang->show_one_barang($this->input->post('id_barang'))->result();         
+               if (count($barang) == 1) {
+                  $data = $this->Model_barang->delete();
+                  http_response_code(202);
+                  $arrResult = array(
+                     'result' => true,
+                     'code' => 202,
+                     'message' => 'Data Was Deleted'
+                  );
+
+               }
+               echo json_encode($arrResult);
+
+             break;
+       }
+    }
 
      public function create()
      {
-         $data = $this->Model_barang->add();
 
-         http_response_code(500);
-         $arrResult = array(
-            'result' => false,
-            'code' => 500,
-            'message' => 'Internal Server Error'
-         );
-
-         if ($data == 1) {
-            http_response_code(201);
-            $arrResult = array(
-               'result' => true,
-               'code' => 201,
-               'message' => 'Data Was Created'
-            );         
-   
-         }
-         echo json_encode($arrResult);
      }
 
       public function update()
       {
-         $data = $this->Model_barang->update();
-         http_response_code(404);
-         $arrResult = array(
-            'result' => false,
-            'code' => 404,
-            'message' => 'Data Not Found'
-         );
-         $barang = $this->Model_barang->show_one_barang($this->input->post('id_barang'))->result();         
-         if (count($barang) == 1) {
-            http_response_code(200);
-            $arrResult = array(
-               'result' => true,
-               'code' => 200,
-               'message' => 'Data Was Updated'
-            );
-         }
-         echo json_encode($arrResult);
       }
 
      public function delete()
      {
-         http_response_code(404);
-         $arrResult = array(
-            'result' => false,
-            'code' => 404,
-            'message' => 'Data Not Found'
-         );
-         $barang = $this->Model_barang->show_one_barang($this->input->post('id_barang'))->result();         
-         if (count($barang) == 1) {
-             $data = $this->Model_barang->delete();
-            http_response_code(202);
-            $arrResult = array(
-               'result' => true,
-               'code' => 202,
-               'message' => 'Data Was Deleted'
-            );
-
-         }
-         echo json_encode($arrResult);
      }
 }
 
